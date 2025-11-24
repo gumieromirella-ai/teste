36cd961a6f6259a71263b60b943626061fa18e18
gumieromirella-ai <gumieromirella@gmail.com>
2025-11-24 16:29:18 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./pages/Home/Home";
 
+const App = () => {
+	return (
+		<>
+			<Header />
+			<div className="flex items-center justify-space-between space-x-5">
+				<Sidebar />
+				<div className="flex flex-col">
+					<Home />
+					{/* <Footer /> */}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default App;
