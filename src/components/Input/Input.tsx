73667397c64b28d928dec0ad3cf2282c22e94e6d
gumieromirella-ai <gumieromirella@gmail.com>
2025-11-24 16:44:03 -0300

    Add files via upload

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,1 +1,16 @@
+interface InputProps {
+	type?: string;
+	placeholder?: string;
+}
 
+const Input = ({ type, placeholder }: InputProps) => {
+	return (
+		<input
+			className="h-full w-full bg-gray rounded-md pl-12 text-sm font-semibold focus: outline-none border border-solid border-[#323b45]"
+			type={type ? type : "text"}
+			placeholder={placeholder}
+		/>
+	);
+};
+
+export default Input;
